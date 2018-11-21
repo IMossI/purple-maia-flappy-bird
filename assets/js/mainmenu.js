34abc74bc1061a80ab8e58985bbd92b6fb31b70e
IMossI <41725345+IMossI@users.noreply.github.com>
2018-11-20 15:29:47 -1000

    Completed Game
    And working

--- assets/js/mainmenu.js
+++ assets/js/mainmenu.js
@@ -6,12 +6,18 @@ var mainMenuState = {
     
     
     preload: function() { 
-        
+        game.load.spritesheet('startBtn', 'assets/images/start_Btn.png', 193, 71);                              
+        game.stage.backgroundColor = '#8185d5';
+        game.scale.pageAlignHorizontally = true;
+        game.scale.pageAlignVertialy = true;
     },
     
     
     create: function() {
+        startBtn = game.add.button(game.world.centerX-95, 200, 'startBtn', this.startGame, this, 2, 1, 0);
         
+        var spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        spaceKey.onDown.add(this.startGame, this);
     },
     
     
@@ -20,7 +26,7 @@ var mainMenuState = {
     },
 
     startGame: function() {
-       // game.state.start('play');
+       game.state.start('play');
     }
 
 };
